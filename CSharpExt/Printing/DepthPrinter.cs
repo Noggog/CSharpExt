2c1ce61813632b070f37a57203c1aa5726e4bb26
Justin Swanson <justin.c.swanson@gmail.com>
2017-01-08 02:28:55 -0600

    Added DepthPrinter

---
+++ CSharpExt/Printing/DepthPrinter.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Noggog.Extensions
+{
+    public class DepthPrinter
+    {
+        internal int depth = 0;
+        public string DepthBuffer = "    ";
+        private List<KeyValuePair<int, List<string>>> list = new List<KeyValuePair<int, List<string>>>();
+
+        public void AddLine(params object[] objs)
+        {
+            List<string> strs = new List<string>(objs.Length);
+            foreach (var o in objs)
+            {
+                strs.Add(o.ToString());
+            }
+            list.Add(new KeyValuePair<int, List<string>>(depth, strs));
+        }
+
+        public void Add(DepthPrinter rhs)
+        {
+            foreach (var line in rhs.list)
+            {
+                list.Add(new KeyValuePair<int, List<string>>(line.Key, new List<string>(line.Value)));
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var line in list)
+            {
+                for (int i = 0; i < line.Key; i++)
+                {
+                    sb.Append(DepthBuffer);
+                }
+                foreach (var s in line.Value)
+                {
+                    sb.Append(s);
+                }
+                sb.Append("\n");
+            }
+            return sb.ToString();
+        }
+
+        public DepthCounter IncrementDepth()
+        {
+            return new DepthCounter(this);
+        }
+
+        public void Shift(int amount)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                var line = list[i];
+                list[i] = new KeyValuePair<int, List<string>>(line.Key + amount, line.Value);
+            }
+        }
+    }
+
+    public class DepthCounter : IDisposable
+    {
+        DepthPrinter printer;
+
+        internal DepthCounter(DepthPrinter printer)
+        {
+            printer.depth++;
+            this.printer = printer;
+        }
+
+        public void Dispose()
+        {
+            printer.depth--;
+        }
+    }
+}
