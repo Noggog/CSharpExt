29df2d11ce14b67281982f4b7439eaea24bd0011
Justin Swanson <justin.c.swanson@gmail.com>
2017-01-12 19:41:03 -0600

    Added xml wrapper classes

---
+++ CSharpExt/XML/DocumentWrapper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Xml;
+
+namespace Noggog.Xml
+{
+    public struct DocumentWrapper : IDisposable
+    {
+        XmlWriter writer;
+
+        public DocumentWrapper(XmlWriter writer)
+        {
+            this.writer = writer;
+            writer.WriteStartDocument();
+        }
+
+        public void Dispose()
+        {
+            writer.WriteEndDocument();
+        }
+    }
+}
---
+++ CSharpExt/XML/ElementWrapper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Xml;
+
+namespace Noggog.Xml
+{
+    public struct ElementWrapper : IDisposable
+    {
+        XmlWriter writer;
+
+        public ElementWrapper(XmlWriter writer, string elem)
+        {
+            this.writer = writer;
+            this.writer.WriteStartElement(elem);
+        }
+
+        public void Dispose()
+        {
+            this.writer.WriteEndElement();
+        }
+    }
+}
