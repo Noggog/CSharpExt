e32e8f77a6ca3ea8f256e94b9d86ee50f38b3c50
Justin Swanson <justin.c.swanson@gmail.com>
2017-04-24 21:18:18 -0500

    ICollectionExt:  Last functions to do something special on the last

--- CSharpExt/Extensions/ICollectionExt.cs
+++ CSharpExt/Extensions/ICollectionExt.cs
@@ -18,5 +18,37 @@ namespace System
                 coll.Add(e);
             }
         }
+
+        public static void Last<T>(
+            this ICollection<T> coll,
+            Action<T> each,
+            Action<T> last)
+        {
+            int i = 0;
+            foreach (var item in coll)
+            {
+                if (i == coll.Count - 1)
+                {
+                    last(item);
+                }
+                else
+                {
+                    each(item);
+                }
+                i++;
+            }
+        }
+
+        public static void Last<T>(
+            this ICollection<T> coll,
+            Action<T, bool> each)
+        {
+            int i = 0;
+            foreach (var item in coll)
+            {
+                each(item, i == coll.Count - 1);
+                i++;
+            }
+        }
     }
 }
