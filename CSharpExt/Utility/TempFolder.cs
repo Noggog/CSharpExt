8ad15eb0920d8225b352c42e758602c59c70b539
Justin Swanson <justin.c.swanson@gmail.com>
2017-08-11 18:23:52 -0500

    TempFolder IDisposable class to streamline temporary folder creation/cleanup

---
+++ CSharpExt/Utility/TempFolder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Noggog.Utility
+{
+    public class TempFolder : IDisposable
+    {
+        public DirectoryInfo Dir { get; private set; }
+
+        public TempFolder()
+        {
+            this.Dir = new DirectoryInfo(Path.Combine(Path.GetTempPath(), Path.GetRandomFileName()));
+            this.Dir.Create();
+            
+        }
+
+        public void Dispose()
+        {
+            this.Dir.DeleteEntireFolder();
+        }
+    }
+}
