26a8ca70dade3276e56f4a0b2e0bbab35e4c1baa
Justin Swanson <justin.c.swanson@gmail.com>
2017-08-15 18:34:17 -0500

    Dictionary.Set(IEnumerable<KeyValuePair>>)

--- CSharpExt/Extensions/DictionaryExt.cs
+++ CSharpExt/Extensions/DictionaryExt.cs
@@ -5,6 +5,14 @@ namespace System
 {
     public static class DictionaryExt
     {
+        public static void Set<K, V>(this IDictionary<K, V> dict, IEnumerable<KeyValuePair<K, V>> items)
+        {
+            foreach (var item in items)
+            {
+                dict[item.Key] = item.Value;
+            }
+        }
+
         public static V TryCreateValue<K, V>(this Dictionary<K, V> dict, K key)
             where V : new()
         {
