d70dc393ca6449f19b3f50697840962a1d416032
Justin Swanson <justin.c.swanson@gmail.com>
2017-12-05 20:49:17 -0600

    TaskExt.Timeout and DoThenComplete

--- CSharpExt/Extensions/TaskExt.cs
+++ CSharpExt/Extensions/TaskExt.cs
@@ -20,5 +20,28 @@ namespace System
             if (t == null) return default(T);
             return (await t).Value;
         }
+
+        public static async Task<bool> Timeout(this Task task, int? timeoutMS, bool throwIfTimeout = false)
+        {
+            if (!timeoutMS.HasValue) return false;
+            var retTask = await Task.WhenAny(task, Task.Delay(timeoutMS.Value));
+            if (retTask == task) return false;
+            if (throwIfTimeout) throw new TimeoutException($"Task took longer than {timeoutMS.Value}ms.");
+            return true;
+        }
+
+        public static async Task DoThenComplete(TaskCompletionSource<bool> tcs, Func<Task> action)
+        {
+            try
+            {
+                await action();
+                tcs?.SetResult(true);
+            }
+            catch (Exception ex)
+            {
+                tcs?.SetException(ex);
+                throw;
+            }
+        }
     }
 }
