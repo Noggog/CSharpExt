33adc063dc20048f22d130af6da96ec15959a025
Justin Swanson <justin.c.swanson@gmail.com>
2017-12-14 21:25:33 -0600

    ICollectionGetter

---
+++ CSharpExt/Containers/Common/ICollectionGetter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Noggog
+{
+    public interface ICollectionGetter<T> : IEnumerable<T>, IEnumerable
+    {
+        int Count { get; }
+        bool Contains(T item);
+        void CopyTo(T[] array, int arrayIndex);
+    }
+
+    public struct CollectionGetterWrapper<T> : ICollectionGetter<T>
+    {
+        private readonly ICollection<T> _source;
+
+        public CollectionGetterWrapper(ICollection<T> source)
+        {
+            this._source = source;
+        }
+
+        public int Count => _source.Count;
+        
+        public bool Contains(T item)
+        {
+            return _source.Contains(item);
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            _source.CopyTo(array, arrayIndex);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _source.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _source.GetEnumerator();
+        }
+    }
+}
