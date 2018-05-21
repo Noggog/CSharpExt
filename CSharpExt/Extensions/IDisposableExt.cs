1a86ba8174f96687bad8be0b8bbc092d2f28b780
Justin Swanson <justin.c.swanson@gmail.com>
2018-05-21 16:33:41 -0500

    IDisposableExt.Nothing

---
+++ CSharpExt/Extensions/IDisposableExt.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Noggog
+{
+    public static class IDisposableExt
+    {
+        public static readonly IDisposable Nothing = new IDisposableNothing();
+
+        class IDisposableNothing : IDisposable
+        {
+            public void Dispose()
+            {
+            }
+        }
+    }
+}
