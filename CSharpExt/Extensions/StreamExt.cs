7a346b58a557f24f7f9288be3a757cfd817af590
Justin Swanson <justin.c.swanson@gmail.com>
2018-07-10 18:37:12 -0500

    StreamExt.Remaining

---
+++ CSharpExt/Extensions/StreamExt.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System
+{
+    public static class StreamExt
+    {
+        public static long Remaining(this Stream stream)
+        {
+            return stream.Length - stream.Position;
+        }
+    }
+}
