3953e64a1bf791d6da27c861a46c6a63c0e88c6a
Justin Swanson <justin.c.swanson@gmail.com>
2018-07-10 18:37:20 -0500

    BasicSubstitutionRangeStream

---
+++ CSharpExt.Tests/BasicSubstitutionStream_Tests.cs
@@ -0,0 +1,116 @@
+﻿using Noggog;
+using Noggog.Streams.Binary;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CSharpExt.Tests
+{
+    public class BasicSubstitutionRangeStream_Tests
+    {
+        public const int TypicalLength = 100;
+        public const byte Substitution = 244;
+
+        public byte[] GetBytes(int len)
+        {
+            byte[] ret = new byte[len];
+            for (int i = 0; i < len; i++)
+            {
+                ret[i] = (byte)i;
+            }
+            return ret;
+        }
+
+        public MemoryStream GetStream(int len)
+        {
+            return new MemoryStream(
+                GetBytes(len));
+        }
+
+        public MemoryStream GetTypicalMemStream()
+        {
+            return GetStream(TypicalLength);
+        }
+
+        public RangeCollection GetTypicalRangeCollection()
+        {
+            var rangeColl = new RangeCollection();
+            rangeColl.Add(
+                new RangeInt64(
+                    4, 12));
+            rangeColl.Add(
+                new RangeInt64(
+                    50, 60));
+            return rangeColl;
+        }
+
+        public BasicSubstitutionRangeStream GetTypical()
+        {
+            return new BasicSubstitutionRangeStream(
+                GetTypicalMemStream(),
+                GetTypicalRangeCollection(),
+                Substitution);
+        }
+
+        [Fact]
+        public void ReadNoSubs()
+        {
+            var stream = new BasicSubstitutionRangeStream(
+                GetTypicalMemStream(),
+                subList: new Noggog.RangeCollection(),
+                toSubstitute: 0);
+            byte[] outBytes = new byte[TypicalLength];
+            Assert.Equal(TypicalLength, stream.Read(outBytes, 0, TypicalLength));
+            for (int i = 0; i < outBytes.Length; i++)
+            {
+                Assert.Equal((byte)i, outBytes[i]);
+            }
+        }
+
+        [Fact]
+        public void Read()
+        {
+            var rangeColl = GetTypicalRangeCollection();
+            var stream = GetTypical();
+            byte[] outBytes = new byte[TypicalLength];
+            Assert.Equal(TypicalLength, stream.Read(outBytes, 0, TypicalLength));
+            for (int i = 0; i < outBytes.Length; i++)
+            {
+                if (rangeColl.IsEncapsulated(i))
+                {
+                    Assert.Equal(Substitution, outBytes[i]);
+                }
+                else
+                {
+                    Assert.Equal((byte)i, outBytes[i]);
+                }
+            }
+        }
+
+        [Fact]
+        public void ReadInRange()
+        {
+            var rangeColl = GetTypicalRangeCollection();
+            var stream = GetTypical();
+            int start = 5;
+            stream.Position = start;
+            byte[] outBytes = new byte[TypicalLength - start];
+            Assert.Equal(TypicalLength - start, stream.Read(outBytes, 0, TypicalLength));
+            for (int i = 0; i < outBytes.Length; i++)
+            {
+                if (rangeColl.IsEncapsulated(i + start))
+                {
+                    Assert.Equal(Substitution, outBytes[i]);
+                }
+                else
+                {
+                    Assert.Equal((byte)(i + start), outBytes[i]);
+                }
+            }
+        }
+    }
+}
---
+++ CSharpExt/Streams/Binary/BasicSubstitutionRangeStream.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Noggog.Streams.Binary
+{
+    public class BasicSubstitutionRangeStream : Stream
+    {
+        private Stream sourceStream;
+        private RangeCollection subList;
+        private byte toSubstitute;
+        
+        public override bool CanRead => true;
+        public override bool CanSeek => true;
+        public override bool CanWrite => false;
+        public override long Length => sourceStream.Length;
+        public override long Position
+        {
+            get => sourceStream.Position;
+            set => sourceStream.Position = value;
+        }
+
+        public BasicSubstitutionRangeStream(
+            Stream sourceStream,
+            RangeCollection subList,
+            byte toSubstitute)
+        {
+            this.toSubstitute = toSubstitute;
+            this.sourceStream = sourceStream;
+            this.subList = subList;
+        }
+
+        public override void Flush()
+        {
+            sourceStream.Flush();
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            count = (int)Math.Min(count, sourceStream.Remaining());
+            int amountRead = 0;
+            while (count > 0)
+            {
+                var pos = sourceStream.Position;
+                if (subList.TryGetCurrentOrNextRange(pos, out var range))
+                {
+                    int diff = (int)Math.Min(count, sourceStream.Length - sourceStream.Position);
+                    if (range.IsInRange(pos))
+                    {
+                        diff = Math.Min(diff, (int)(range.Max - pos + 1));
+                        for (int i = 0; i < diff; i++)
+                        {
+                            buffer[i + offset] = toSubstitute;
+                        }
+                        sourceStream.Position += diff;
+                    }
+                    else
+                    {
+                        diff = Math.Min(diff, (int)(range.Min - pos));
+                        sourceStream.Read(buffer, offset, diff);
+                    }
+                    offset += diff;
+                    count -= diff;
+                    amountRead += diff;
+                }
+                else
+                {
+                    amountRead += sourceStream.Read(buffer, offset, count);
+                    break;
+                }
+            }
+            return amountRead;
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            return sourceStream.Seek(offset, origin);
+        }
+
+        public override void SetLength(long value)
+        {
+            sourceStream.SetLength(value);
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
