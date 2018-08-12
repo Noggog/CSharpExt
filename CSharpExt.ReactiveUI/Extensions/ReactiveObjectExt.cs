cae0464703fdca048642ea97a0e2a8a56011cd7e
Justin Swanson <justin.c.swanson@gmail.com>
2018-08-12 16:41:15 -0500

    CSharpExt.ReactiveUI

---
+++ CSharpExt.ReactiveUI/Extensions/ReactiveObjectExt.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReactiveUI
+{
+    public static class ReactiveObjectExt
+    {
+        public static void RaiseAndSetIfChanged<T>(
+            this ReactiveObject reactiveObj,
+            ref T item,
+            T newItem,
+            ref bool hasBeenSet,
+            bool newHasBeenSet,
+            string name,
+            string hasBeenSetName)
+        {
+            if (!newHasBeenSet)
+            {
+                reactiveObj.RaiseAndSetIfChanged(ref hasBeenSet, newHasBeenSet, propertyName: hasBeenSetName);
+            }
+            reactiveObj.RaiseAndSetIfChanged(ref item, newItem, propertyName: name);
+            if (newHasBeenSet)
+            {
+                reactiveObj.RaiseAndSetIfChanged(ref hasBeenSet, newHasBeenSet, propertyName: hasBeenSetName);
+            }
+        }
+    }
+}
