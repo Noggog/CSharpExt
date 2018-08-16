b8752ce5bfe2a6940eac774bbb6b3315fa7b4931
Justin Swanson <justin.c.swanson@gmail.com>
2018-08-15 21:30:34 -0500

    ReactiveObjectExt.RaiseAndSetIfChanged with BitArray convenience

--- CSharpExt.ReactiveUI/Extensions/ReactiveObjectExt.cs
+++ CSharpExt.ReactiveUI/Extensions/ReactiveObjectExt.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
@@ -27,6 +28,49 @@ namespace ReactiveUI
             {
                 reactiveObj.RaiseAndSetIfChanged(ref hasBeenSet, newHasBeenSet, propertyName: hasBeenSetName);
             }
+        }
+
+        public static void RaiseAndSetIfChanged<T>(
+            this ReactiveObject reactiveObj,
+            ref T item,
+            T newItem,
+            BitArray hasBeenSet,
+            bool newHasBeenSet,
+            int index,
+            string name,
+            string hasBeenSetName)
+        {
+            var oldHasBeenSet = hasBeenSet[index];
+            bool itemEqual = EqualityComparer<T>.Default.Equals(item, newItem);
+            if (oldHasBeenSet != newHasBeenSet)
+            {
+                reactiveObj.RaisePropertyChanging(hasBeenSetName);
+                hasBeenSet[index] = newHasBeenSet;
+            }
+            if (!itemEqual)
+            {
+                reactiveObj.RaisePropertyChanging(name);
+                item = newItem;
+                reactiveObj.RaisePropertyChanged(name);
+            }
+            if (oldHasBeenSet != newHasBeenSet)
+            {
+                reactiveObj.RaisePropertyChanged(hasBeenSetName);
+            }
+        }
+
+        public static void RaiseAndSetIfChanged(
+            this ReactiveObject reactiveObj,
+            BitArray hasBeenSet,
+            bool newHasBeenSet,
+            int index,
+            string name)
+        {
+            var oldHasBeenSet = hasBeenSet[index];
+            if (oldHasBeenSet = newHasBeenSet) return;
+            reactiveObj.RaisePropertyChanging(name);
+            hasBeenSet[index] = newHasBeenSet;
+            reactiveObj.RaisePropertyChanged(name);
         }
 
         public static IObservable<TRet> WhenAny<TSender, TRet>(
