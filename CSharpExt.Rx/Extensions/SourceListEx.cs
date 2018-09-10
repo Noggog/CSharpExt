e68fad189811053e1e5beb8054bb63fb281e700c
Justin Swanson <justin.c.swanson@gmail.com>
2018-09-10 08:12:22 -0500

    SourceListExt.SetTo

---
+++ CSharpExt.Rx/Extensions/SourceListEx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicData
+{
+    public static class SourceListEx
+    {
+        public static void SetTo<T>(this SourceList<T> list, IEnumerable<T> items)
+        {
+            list.Edit((l) =>
+            {
+                l.SetTo(items);
+            });
+        }
+    }
+}
