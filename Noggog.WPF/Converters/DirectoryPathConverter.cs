63ddba9655f4e1d8efe5f8db2ec7244f774988d1
Justin Swanson <justin.c.swanson@gmail.com>
2019-04-07 13:22:08 -0500

    Some typical WPF converters

---
+++ Noggog.WPF/Converters/DirectoryPathConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Noggog.WPF
+{
+    [ValueConversion(typeof(DirectoryPath), typeof(string))]
+    public class DirectoryPathConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (targetType != typeof(string))
+                throw new InvalidOperationException($"The target must be of type string");
+            return ((DirectoryPath)value).Path;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (targetType != typeof(DirectoryPath))
+                throw new InvalidOperationException($"The target must be of type {nameof(DirectoryPath)}");
+            return new DirectoryPath((string)value);
+        }
+    }
+}
