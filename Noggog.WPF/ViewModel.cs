f582339ab4bf7cf31e9a6f9e6894a0eeb14795ca
Justin Swanson <justin.c.swanson@gmail.com>
2019-05-07 23:10:47 -0500

    Added a ViewModel convenience class /w composite subscription

---
+++ Noggog.WPF/ViewModel.cs
@@ -0,0 +1,24 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reactive.Disposables;
+
+namespace Noggog.WPF
+{
+    public class ViewModel : ReactiveObject, IDisposable
+    {
+        private readonly Lazy<CompositeDisposable> _CompositeDisposable = new Lazy<CompositeDisposable>();
+        public CompositeDisposable CompositeDisposable => _CompositeDisposable.Value;
+
+        public virtual void Dispose()
+        {
+            if (_CompositeDisposable.IsValueCreated)
+            {
+                _CompositeDisposable.Value.Dispose();
+            }
+        }
+    }
+}
