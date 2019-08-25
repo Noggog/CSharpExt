355910af30339bc70d9e47f7af9b20168f8a9c86
Justin Swanson <justin.c.swanson@gmail.com>
2019-08-25 18:48:12 -0500

    ViewModel:  Extra RaiseAndSet calls

--- Noggog.WPF/ViewModel.cs
+++ Noggog.WPF/ViewModel.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Reactive.Disposables;
+using System.Runtime.CompilerServices;
+using System.Collections;
 
 namespace Noggog.WPF
 {
@@ -19,6 +21,134 @@ namespace Noggog.WPF
             {
                 _CompositeDisposable.Value.Dispose();
             }
+        }
+        
+        protected void RaiseAndSetIfChanged<T>(
+            ref T item,
+            T newItem,
+            [CallerMemberName] string propertyName = null)
+            where T : struct
+        {
+            if (EqualityComparer<T>.Default.Equals(item, newItem)) return;
+            item = newItem;
+            this.RaisePropertyChanged(propertyName);
+        }
+
+        protected void RaiseAndSetIfReferenceChanged<T>(
+            ref T item,
+            T newItem,
+            [CallerMemberName] string propertyName = null)
+            where T : class
+        {
+            if (ReferenceEquals(item, newItem)) return;
+            item = newItem;
+            this.RaisePropertyChanged(propertyName);
+        }
+
+        protected void RaiseAndSetIfChanged<T>(
+            ref T item,
+            T newItem,
+            ref bool hasBeenSet,
+            bool newHasBeenSet,
+            string name,
+            string hasBeenSetName)
+            where T : struct
+        {
+            if (!newHasBeenSet)
+            {
+                this.RaiseAndSetIfChanged(ref hasBeenSet, newHasBeenSet, propertyName: hasBeenSetName);
+            }
+            this.RaiseAndSetIfChanged(ref item, newItem, propertyName: name);
+            if (newHasBeenSet)
+            {
+                this.RaiseAndSetIfChanged(ref hasBeenSet, newHasBeenSet, propertyName: hasBeenSetName);
+            }
+        }
+
+        protected void RaiseAndSetIfReferenceChanged<T>(
+            ref T item,
+            T newItem,
+            ref bool hasBeenSet,
+            bool newHasBeenSet,
+            string name,
+            string hasBeenSetName)
+            where T : class
+        {
+            if (!newHasBeenSet)
+            {
+                this.RaiseAndSetIfChanged(ref hasBeenSet, newHasBeenSet, propertyName: hasBeenSetName);
+            }
+            this.RaiseAndSetIfReferenceChanged(ref item, newItem, propertyName: name);
+            if (newHasBeenSet)
+            {
+                this.RaiseAndSetIfChanged(ref hasBeenSet, newHasBeenSet, propertyName: hasBeenSetName);
+            }
+        }
+
+        protected void RaiseAndSetIfChanged<T>(
+            ref T item,
+            T newItem,
+            BitArray hasBeenSet,
+            bool newHasBeenSet,
+            int index,
+            string name,
+            string hasBeenSetName)
+            where T : struct
+        {
+            var oldHasBeenSet = hasBeenSet[index];
+            bool itemEqual = EqualityComparer<T>.Default.Equals(item, newItem);
+            if (oldHasBeenSet != newHasBeenSet)
+            {
+                hasBeenSet[index] = newHasBeenSet;
+            }
+            if (!itemEqual)
+            {
+                item = newItem;
+                this.RaisePropertyChanged(name);
+            }
+            if (oldHasBeenSet != newHasBeenSet)
+            {
+                this.RaisePropertyChanged(hasBeenSetName);
+            }
+        }
+
+        protected void RaiseAndSetIfReferenceChanged<T>(
+            ref T item,
+            T newItem,
+            BitArray hasBeenSet,
+            bool newHasBeenSet,
+            int index,
+            string name,
+            string hasBeenSetName)
+            where T : class
+        {
+            var oldHasBeenSet = hasBeenSet[index];
+            bool itemEqual = ReferenceEquals(item, newItem);
+            if (oldHasBeenSet != newHasBeenSet)
+            {
+                hasBeenSet[index] = newHasBeenSet;
+            }
+            if (!itemEqual)
+            {
+                item = newItem;
+                this.RaisePropertyChanged(name);
+            }
+            if (oldHasBeenSet != newHasBeenSet)
+            {
+                this.RaisePropertyChanged(hasBeenSetName);
+            }
+        }
+
+        protected void RaiseAndSetIfChanged(
+            BitArray hasBeenSet,
+            bool newHasBeenSet,
+            int index,
+            string name)
+        {
+            var oldHasBeenSet = hasBeenSet[index];
+            if (oldHasBeenSet == newHasBeenSet) return;
+            hasBeenSet[index] = newHasBeenSet;
+            this.RaisePropertyChanged(name);
         }
     }
 }
