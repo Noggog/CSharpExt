3e4a666ae4a538ab7dfd0d0acdbb64d54f0d5ac7
Justin Swanson <justin.c.swanson@gmail.com>
2019-09-01 01:04:53 -0500

    ObservableListExt.TransformMany /w observable selector

--- CSharpExt/Rx/Extensions/ObservableListExt.cs
+++ CSharpExt/Rx/Extensions/ObservableListExt.cs
@@ -1,4 +1,5 @@
 ﻿using DynamicData;
+using DynamicData.Annotations;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +13,20 @@ namespace System
         public static IObservable<IChangeSet<TObject>> DistinctValues<TObject>(this IObservable<IChangeSet<TObject>> source)
         {
             return DynamicData.ObservableListEx.DistinctValues(source, i => i);
+        }
+
+        public static IObservable<IChangeSet<TDestination>> TransformMany<TDestination, TSource>([NotNull] this IObservable<IChangeSet<TSource>> source,
+            [NotNull] Func<TSource, IObservable<TDestination>> manyselector,
+            IEqualityComparer<TDestination> equalityComparer = null)
+        {
+            return source
+                .TransformMany(i =>
+                {
+                    return manyselector(i)
+                        .ToObservableChangeSet()
+                        .AsObservableList();
+                },
+                equalityComparer: equalityComparer);
         }
     }
 }
