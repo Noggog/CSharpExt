3070d37cd5f8bc229d375aeea7905f2e09717479
Justin Swanson <justin.c.swanson@gmail.com>
2019-09-20 20:49:18 -0500

    WindowExt.WireMainVM

---
+++ Noggog.WPF/Extensions/WindowExt.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Noggog.WPF
+{
+    public static class WindowExt
+    {
+        public static void WireMainVM<T>(
+            this Window window,
+            T mainVM,
+            string settingsPath,
+            Action<string, T> load,
+            Action<string, T> save)
+        {
+            window.Closed += (a, b) =>
+            {
+                FilePath filePath = new FilePath(settingsPath);
+                filePath.Directory.Create();
+                save(settingsPath, mainVM);
+            };
+            window.DataContext = mainVM;
+            load(settingsPath, mainVM);
+        }
+    }
+}
