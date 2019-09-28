cfc205e3661e149d21853a53801d9c94cbf88804
Justin Swanson <justin.c.swanson@gmail.com>
2019-09-28 11:26:41 -0500

    DynamicData.ExtendedList implementation

---
+++ CSharpExt/Containers/ExtendedList.cs
@@ -0,0 +1,25 @@
+﻿using DynamicData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Noggog
+{
+    public class ExtendedList<T> : List<T>, IExtendedList<T>
+    {
+        public void InsertRange(IEnumerable<T> collection, int index)
+        {
+            foreach (var item in collection.Reverse())
+            {
+                this.Insert(index, item);
+            }
+        }
+
+        public void Move(int original, int destination)
+        {
+            this[destination] = this[original];
+            this[original] = default;
+        }
+    }
+}
