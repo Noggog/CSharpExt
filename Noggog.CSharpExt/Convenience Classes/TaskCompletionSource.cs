1f7fcabc4d03bbbe9663b712f12bd1b5d32975b3
Justin Swanson <justin.c.swanson@gmail.com>
2020-06-03 19:02:46 -0500

    TaskCompletionSource.TryComplete

--- Noggog.CSharpExt/Convenience Classes/TaskCompletionSource.cs
+++ Noggog.CSharpExt/Convenience Classes/TaskCompletionSource.cs
@@ -18,5 +18,14 @@ namespace Noggog
         {
             this.SetResult(true);
         }
+
+        /// <summary>
+        /// Transitions the underlying Task into the RanToCompletion state if it can.
+        /// Will fail to do so if the task is already complete or is faulted.
+        /// </summary>
+        public bool TryComplete()
+        {
+            return this.TrySetResult(true);
+        }
     }
 }
