07e4bdb230baf06ea54d5433d37560889288d0f5
Justin Swanson <justin.c.swanson@gmail.com>
2020-07-26 02:34:18 -0500

    NoggogUserControl
    Just forwards the DataContext to the ViewModel automatically

---
+++ Noggog.WPF/NoggogUserControl.cs
@@ -0,0 +1,26 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Noggog.WPF
+{
+    public class NoggogUserControl<TViewModel> : ReactiveUserControl<TViewModel>
+        where TViewModel : class
+    {
+        public NoggogUserControl()
+        {
+            this.DataContextChanged += (o, e) =>
+            {
+                if (e.NewValue is TViewModel vm)
+                {
+                    this.ViewModel = vm;
+                }
+                else
+                {
+                    this.ViewModel = null!;
+                }
+            };
+        }
+    }
+}
