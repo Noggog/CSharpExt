737d739d7576412bed1189b7f38e35754118102f
Justin Swanson <justin.c.swanson@gmail.com>
2020-08-10 12:05:08 -0500

    CommandExt.Starting/EndingExecution observables

---
+++ Noggog.WPF/Extensions/CommandExt.cs
@@ -0,0 +1,29 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Text;
+
+namespace Noggog.WPF
+{
+    public static class CommandExt
+    {
+        public static IObservable<Unit> StartingExecution(this IReactiveCommand cmd)
+        {
+            return cmd.IsExecuting
+                .DistinctUntilChanged()
+                .Where(x => x)
+                .Unit();
+        }
+
+        public static IObservable<Unit> EndingExecution(this IReactiveCommand cmd)
+        {
+            return cmd.IsExecuting
+                .DistinctUntilChanged()
+                .Pairwise()
+                .Where(x => x.Previous && !x.Current)
+                .Unit();
+        }
+    }
+}
