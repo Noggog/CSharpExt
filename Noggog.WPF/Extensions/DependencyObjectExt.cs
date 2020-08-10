e6ac706fe7b4541088eb26ffbd514e54e5bea133
Justin Swanson <justin.c.swanson@gmail.com>
2020-08-10 07:36:56 -0500

    DependencyObjectExt.TryGetAncestor

---
+++ Noggog.WPF/Extensions/DependencyObjectExt.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Noggog.WPF
+{
+    public static class DependencyObjectExt
+    {
+        public static bool TryGetAncestor<TObj>(this DependencyObject obj, [MaybeNullWhen(false)] out TObj foundObj, bool testSelf = true)
+            where TObj : DependencyObject
+        {
+            DependencyObject? item = obj;
+            while (item != null && !(item is TObj))
+                item = VisualTreeHelper.GetParent(item);
+            foundObj = item as TObj;
+            return foundObj != null;
+        }
+
+        public static TObj? GetAncestor<TObj>(this DependencyObject obj, bool testSelf = true)
+            where TObj : DependencyObject
+        {
+            if (TryGetAncestor<TObj>(obj, out var ancestor))
+            {
+                return ancestor;
+            }
+            return default;
+        }
+    }
+}
