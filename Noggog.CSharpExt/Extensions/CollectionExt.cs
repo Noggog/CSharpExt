a918f54eb80b59b41f2371eee6eaf0e304c9e9ad
Justin Swanson <justin.c.swanson@gmail.com>
2020-09-01 13:08:50 -0500

    CollectionExt.AddReturn

---
+++ Noggog.CSharpExt/Extensions/CollectionExt.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Noggog
+{
+    public static class CollectionExt
+    {
+        public static T AddReturn<T>(this ICollection<T> coll, T item)
+        {
+            coll.Add(item);
+            return item;
+        }
+    }
+}
