f9ba8663f01a66093dc4cf583f3d3529641c4992
Justin Swanson <justin.c.swanson@gmail.com>
2020-12-07 15:16:18 -0600

    CommandExt.CreateCombinedAny

--- Noggog.WPF/Extensions/CommandExt.cs
+++ Noggog.WPF/Extensions/CommandExt.cs
@@ -1,9 +1,12 @@
-﻿using ReactiveUI;
+using ReactiveUI;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reactive;
 using System.Reactive.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Noggog.WPF
 {
@@ -24,6 +27,23 @@ namespace Noggog.WPF
                 .Pairwise()
                 .Where(x => x.Previous && !x.Current)
                 .Unit();
+        }
+
+        public static ReactiveCommand<Unit, Unit> CreateCombinedAny(params ReactiveCommand<Unit, Unit>[] commands)
+        {
+
+            return ReactiveCommand.CreateFromTask(
+                execute: () =>
+                {
+                    return Task.WhenAll(commands.Select(async c =>
+                    {
+                        if (((ICommand)c).CanExecute(Unit.Default))
+                        {
+                            await c.Execute();
+                        }
+                    }));
+                },
+                canExecute: Noggog.ObservableExt.Any(commands.Select(x => x.CanExecute).ToArray()));
         }
     }
 }
