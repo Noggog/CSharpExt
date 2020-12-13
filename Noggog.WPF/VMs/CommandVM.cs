41f7bb9c9634029293aa161963f3fb21570d02ed
Justin Swanson <justin.c.swanson@gmail.com>
2020-12-13 15:00:56 -0600

    CommandVM

---
+++ Noggog.WPF/VMs/CommandVM.cs
@@ -0,0 +1,29 @@
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Reactive;
+using System.Text;
+
+namespace Noggog.WPF
+{
+    public class CommandVM : ViewModel
+    {
+        private readonly ObservableAsPropertyHelper<bool> _CanExecute;
+        public bool CanExecute => _CanExecute.Value;
+
+        public ReactiveCommand<Unit, Unit> Command { get; private set; }
+
+        private CommandVM(ReactiveCommand<Unit, Unit> cmd)
+        {
+            Command = cmd;
+            _CanExecute = cmd.CanExecute
+                .ToProperty(this, nameof(CanExecute), false);
+        }
+
+        public static CommandVM Factory(ReactiveCommand<Unit, Unit> cmd)
+        {
+            return new CommandVM(cmd);
+        }
+    }
+}
