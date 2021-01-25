8f20dc93211dd9cfe55484e8195ebf4860e0b296
Justin Swanson <justin.c.swanson@gmail.com>
2021-01-24 18:07:29 -0600

    NoggogControl

---
+++ Noggog.WPF/NoggogControl.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Noggog.WPF
+{
+    public class NoggogControl : Control
+    {
+        protected readonly CompositeDisposable _unloadDisposable = new CompositeDisposable();
+        protected readonly CompositeDisposable _templateDisposable = new CompositeDisposable();
+
+        public NoggogControl()
+        {
+            this.Unloaded += (_, _) =>
+            {
+                _templateDisposable.Dispose();
+                _unloadDisposable.Dispose();
+            };
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            _templateDisposable.Dispose();
+        }
+    }
+}
