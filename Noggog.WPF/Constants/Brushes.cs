8dd38b149379f8198a1c8054d900c6931578b8af
Justin Swanson <justin.c.swanson@gmail.com>
2021-02-13 16:14:41 -0600

    Brush/Color name constants

---
+++ Noggog.WPF/Constants/Brushes.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Noggog.WPF.Brushes
+{
+    public static class Constants
+    {
+        public const string ForegroundDark = "ForegroundDarkBrush";
+        public const string Background = "BackgroundBrush";
+        public const string BackgroundLight = "BackgroundLightBrush";
+        public const string BackgroundDark = "BackgroundDarkBrush";
+        public const string Passive = "PassiveBrush";
+        public const string Yellow = "YellowBrush";
+        public const string YellowDark = "YellowDarkBrush";
+        public const string Green = "GreenBrush";
+        public const string GreenBackground = "GreenBackgroundBrush";
+        public const string Red = "RedBrush";
+        public const string RedBackground = "RedBackgroundBrush";
+        public const string Warning = "WarningBrush";
+        public const string WarningDark = "WarningDarkBrush";
+        public const string Success = "SuccessBrush";
+        public const string Error = "ErrorBrush";
+        public const string ErrorLight = "ErrorLightBrush";
+        public const string ErrorPressed = "ErrorPressedBrush";
+        public const string ErrorBackground = "ErrorBackgroundBrush";
+        public const string ControlFocus = "ControlFocusBrush";
+        public const string SubtleBorder = "SubtleBorderBrush";
+        public const string ControlsDisabled = "ControlsDisabledBrush";
+        public const string ProcessingNeon = "ProcessingNeonBrush";
+        public const string Processing = "ProcessingBrush";
+    }
+}
---
+++ Noggog.WPF/Constants/Colors.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Noggog.WPF.Colors
+{
+    public static class Constants
+    {
+        public const string Yellow = "Yellow";
+        public const string YellowDark = "YellowDark";
+        public const string Red = "Red";
+        public const string RedBackground = "RedBackground";
+        public const string PinkRedForeground = "PinkRedForeground";
+        public const string Green = "Green";
+        public const string FocusedBackground = "FocusedBackground";
+        public const string GreenBackground = "GreenBackground";
+        public const string Success = "Success";
+        public const string Passive = "Passive";
+        public const string Error = "Error";
+        public const string ErrorLight = "ErrorLight";
+        public const string ErrorDark = "ErrorDark";
+        public const string ErrorHover = "ErrorHover";
+        public const string ErrorPressed = "ErrorPressed";
+        public const string Warning = "Warning";
+        public const string WarningDark = "WarningDark";
+        public const string ProcessingNeon = "ProcessingNeon";
+        public const string Processing = "Processing";
+    }
+}
