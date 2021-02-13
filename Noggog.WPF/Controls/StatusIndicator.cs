91b8aeb168633b7a17012229dea970618489ad1e
Justin Swanson <justin.c.swanson@gmail.com>
2021-02-13 16:15:06 -0600

    StatusIndicator

---
+++ Noggog.WPF/Controls/StatusIndicator.cs
@@ -0,0 +1,98 @@
+using ReactiveUI;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Noggog.WPF
+{
+    public class StatusIndicator : Control
+    {
+        public ErrorResponse? Status
+        {
+            get => (ErrorResponse?)GetValue(StatusProperty);
+            set => SetValue(StatusProperty, value);
+        }
+        public static readonly DependencyProperty StatusProperty = DependencyProperty.Register(nameof(Status), typeof(ErrorResponse?), typeof(StatusIndicator),
+             new FrameworkPropertyMetadata(default(ErrorResponse?)));
+
+        public Brush ErrorBrush
+        {
+            get => (Brush)GetValue(ErrorBrushProperty);
+            set => SetValue(ErrorBrushProperty, value);
+        }
+        public static readonly DependencyProperty ErrorBrushProperty = DependencyProperty.Register(nameof(ErrorBrush), typeof(Brush), typeof(StatusIndicator),
+             new FrameworkPropertyMetadata(Application.Current.Resources[Brushes.Constants.Error]));
+
+        public Brush SuccessBrush
+        {
+            get => (Brush)GetValue(SuccessBrushProperty);
+            set => SetValue(SuccessBrushProperty, value);
+        }
+        public static readonly DependencyProperty SuccessBrushProperty = DependencyProperty.Register(nameof(SuccessBrush), typeof(Brush), typeof(StatusIndicator),
+             new FrameworkPropertyMetadata(Application.Current.Resources[Brushes.Constants.Success]));
+
+        public Brush PassiveBrush
+        {
+            get => (Brush)GetValue(PassiveBrushProperty);
+            set => SetValue(PassiveBrushProperty, value);
+        }
+        public static readonly DependencyProperty PassiveBrushProperty = DependencyProperty.Register(nameof(PassiveBrush), typeof(Brush), typeof(StatusIndicator),
+             new FrameworkPropertyMetadata(Application.Current.Resources[Brushes.Constants.Passive]));
+
+        public Color ProcessingSpinnerGlow
+        {
+            get => (Color)GetValue(ProcessingSpinnerGlowProperty);
+            set => SetValue(ProcessingSpinnerGlowProperty, value);
+        }
+        public static readonly DependencyProperty ProcessingSpinnerGlowProperty = DependencyProperty.Register(nameof(ProcessingSpinnerGlow), typeof(Color), typeof(StatusIndicator),
+             new FrameworkPropertyMetadata(Application.Current.Resources[Colors.Constants.Processing]));
+
+        public Brush ProcessingSpinnerForeground
+        {
+            get => (Brush)GetValue(ProcessingSpinnerForegroundProperty);
+            set => SetValue(ProcessingSpinnerForegroundProperty, value);
+        }
+        public static readonly DependencyProperty ProcessingSpinnerForegroundProperty = DependencyProperty.Register(nameof(ProcessingSpinnerForeground), typeof(Brush), typeof(StatusIndicator),
+             new FrameworkPropertyMetadata(Application.Current.Resources[Brushes.Constants.Processing]));
+
+        public bool Processing
+        {
+            get => (bool)GetValue(ProcessingProperty);
+            set => SetValue(ProcessingProperty, value);
+        }
+        public static readonly DependencyProperty ProcessingProperty = DependencyProperty.Register(nameof(Processing), typeof(bool), typeof(StatusIndicator),
+             new FrameworkPropertyMetadata(default(bool)));
+
+        public string ProcessingTooltip
+        {
+            get => (string)GetValue(ProcessingTooltipProperty);
+            set => SetValue(ProcessingTooltipProperty, value);
+        }
+        public static readonly DependencyProperty ProcessingTooltipProperty = DependencyProperty.Register(nameof(ProcessingTooltip), typeof(string), typeof(StatusIndicator),
+             new FrameworkPropertyMetadata(default(string)));
+
+        public ICommand ClickCommand
+        {
+            get => (ICommand)GetValue(ClickCommandProperty);
+            set => SetValue(ClickCommandProperty, value);
+        }
+        public static readonly DependencyProperty ClickCommandProperty = DependencyProperty.Register(nameof(ClickCommand), typeof(ICommand), typeof(StatusIndicator),
+             new FrameworkPropertyMetadata(default(ICommand), FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        static StatusIndicator()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(StatusIndicator), new FrameworkPropertyMetadata(typeof(StatusIndicator)));
+        }
+
+        public StatusIndicator()
+        {
+            ClickCommand = ReactiveCommand.Create(() =>
+            {
+                var reason = Status?.Reason;
+                if (reason == null) return;
+                Clipboard.SetText(reason);
+            });
+        }
+    }
+}
