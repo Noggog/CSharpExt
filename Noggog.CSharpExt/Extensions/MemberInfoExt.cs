fafd9aa1e1e7a16b5c50ebea08b48d1f85eb14a9
Justin Swanson <justin.c.swanson@gmail.com>
2021-02-22 17:03:47 -0600

    MemberInfoExt.TryGetCustomAttribute

---
+++ Noggog.CSharpExt/Extensions/MemberInfoExt.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Noggog
+{
+    public static class MemberInfoExt
+    {
+        public static bool TryGetCustomAttribute<T>(this MemberInfo memberInfo, [MaybeNullWhen(false)] out T attr)
+            where T : Attribute
+        {
+            attr = memberInfo.GetCustomAttribute<T>();
+            return attr != null;
+        }
+    }
+}
