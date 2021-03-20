e9d365909c149632a4aec1e2741a5d94418b5540
Justin Swanson <justin.c.swanson@gmail.com>
2021-03-20 01:14:02 -0500

    MemberInfo.IsStatic

--- Noggog.CSharpExt/Extensions/MemberInfoExt.cs
+++ Noggog.CSharpExt/Extensions/MemberInfoExt.cs
@@ -16,5 +16,11 @@ namespace Noggog
             attr = memberInfo.GetCustomAttribute<T>();
             return attr != null;
         }
+
+        public static bool IsStatic(this PropertyInfo memberInfo)
+        {
+            var method = (memberInfo.GetMethod ?? memberInfo.SetMethod)!;
+            return method.IsStatic;
+        }
     }
 }
