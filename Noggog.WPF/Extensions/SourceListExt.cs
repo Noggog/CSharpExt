509b7affbdeb7fd5e3575ca4f9ca9bd207bf75cf
Justin Swanson <justin.c.swanson@gmail.com>
2021-05-26 21:05:21 -0500

    SourceListExt.SetTo

---
+++ Noggog.WPF/Extensions/SourceListExt.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DynamicData;
+
+namespace Noggog.WPF
+{
+    public static class SourceListExt
+    {
+        public static void SetTo<T>(this ISourceList<T> list, IEnumerable<T> items, bool checkEquality = false)
+        {
+            list.Edit(l =>
+            {
+                int i = 0;
+                foreach (var item in items)
+                {
+                    if (i >= l.Count)
+                    {
+                        l.Add(item);
+                    }
+                    else if (checkEquality)
+                    {
+                        if (!EqualityComparer<T>.Default.Equals(l[i], item))
+                        {
+                            l[i] = item;
+                        }
+                    }
+                    else
+                    {
+                        l[i] = item;
+                    }
+                    i++;
+                }
+
+                l.RemoveToCount(i);
+            });
+        }
+
+        public static void RemoveToCount<T>(this ISourceList<T> list, int count)
+        {
+            list.Edit(l =>
+            {
+                var toRemove = l.Count - count;
+                for (; toRemove > 0; toRemove--)
+                {
+                    l.RemoveAt(l.Count - 1);
+                }
+            });
+        }
+    }
+}
