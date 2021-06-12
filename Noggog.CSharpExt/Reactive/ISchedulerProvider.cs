9ca2a61e1a652dde83ad3eecb076b2cf77702104
Justin Swanson <justin.c.swanson@gmail.com>
2021-06-12 16:04:47 -0500

    ISchedulerProvider

---
+++ Noggog.CSharpExt/Reactive/ISchedulerProvider.cs
@@ -0,0 +1,10 @@
+﻿using System.Reactive.Concurrency;
+
+namespace Noggog.Reactive
+{
+    public interface ISchedulerProvider
+    {
+        public IScheduler MainThread { get; }
+        public IScheduler TaskPool { get; }
+    }
+}
---
+++ Noggog.WPF/SchedulerProvider.cs
@@ -0,0 +1,12 @@
+﻿using System.Reactive.Concurrency;
+using Noggog.Reactive;
+using ReactiveUI;
+
+namespace Noggog.WPF
+{
+    public class SchedulerProvider : ISchedulerProvider
+    {
+        public IScheduler MainThread => RxApp.MainThreadScheduler;
+        public IScheduler TaskPool  => RxApp.TaskpoolScheduler;
+    }
+}
