2bcee6440a58147b93e0c5835e0e9b29b3716311
Justin Swanson <justin.c.swanson@gmail.com>
2021-07-03 14:03:10 -0500

    Noggog.Testing /w ITestableObservable ext

---
+++ Noggog.Testing/Extensions/TestableObservableExt.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using System.Reactive;
+using FluentAssertions;
+using Microsoft.Reactive.Testing;
+
+namespace Noggog
+{
+    public static class TestableObservableExt
+    {
+        public static void ShouldHaveNoErrors<T>(this ITestableObservable<T> obs)
+        {
+            obs.Messages.Where(x => x.Value.Kind == NotificationKind.OnError)
+                .Should()
+                .BeEmpty();
+        }
+        
+        public static void ShouldNotBeCompleted<T>(this ITestableObservable<T> obs)
+        {
+            obs.Messages.Where(x => x.Value.Kind == NotificationKind.OnCompleted)
+                .Should()
+                .BeEmpty();
+        }
+    }
+}
