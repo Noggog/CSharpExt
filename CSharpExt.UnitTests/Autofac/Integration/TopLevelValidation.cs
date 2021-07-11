ad6dfe2e177dcff4365ef66befde004f3e7ceda5
Justin Swanson <justin.c.swanson@gmail.com>
2021-07-11 00:40:21 -0500

    TopLevelValidation integration test

---
+++ CSharpExt.UnitTests/Autofac/Integration/TopLevelValidation.cs
@@ -0,0 +1,35 @@
+﻿using Autofac;
+using Noggog.Autofac.Validation;
+using Xunit;
+
+namespace CSharpExt.UnitTests.Autofac.Integration
+{
+    public class TopLevelValidation : IClassFixture<ValidationFixture>
+    {
+        private readonly ValidationFixture _validationFixture;
+
+        public TopLevelValidation(ValidationFixture validationFixture)
+        {
+            _validationFixture = validationFixture;
+        }
+
+        interface IOtherClass {}
+        record OtherClass() : IOtherClass;
+
+        interface ITopLevel {}
+        record TopLevel(IOtherClass OtherClass) : ITopLevel;
+
+        [Fact]
+        public void Interface()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<TopLevel>().As<ITopLevel>();
+            var cont = builder.Build();
+            Assert.Throws<AutofacValidationException>(() =>
+            {
+                using var disp = _validationFixture.GetValidator(cont, out var validate);
+                validate.Validate(typeof(ITopLevel));
+            });
+        }
+    }
+}
