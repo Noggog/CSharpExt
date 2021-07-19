f828ed9c26c7c07569c28a99d24511ca2f72ff92
Justin Swanson <justin.c.swanson@gmail.com>
2021-07-19 17:01:45 -0500

    NSubstitute ReturnEmpty

---
+++ Noggog.Testing/Extensions/NSubstituteExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Reactive.Linq;
+using NSubstitute;
+using NSubstitute.Core;
+
+namespace Noggog.NSubstitute
+{
+    public static class NSubstituteExtensions
+    {
+        public static ConfiguredCall ReturnEmpty<T>(this IObservable<T> obs)
+        {
+            return obs.Returns(Observable.Empty<T>());
+        }
+    }
+}
