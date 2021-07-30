36221713b7889d4a8aa4f64a436e9b691d622970
Justin Swanson <justin.c.swanson@gmail.com>
2021-07-30 18:51:38 -0500

    Noggog.IO.DeleteEntireDirectory

---
+++ Noggog.CSharpExt/IO/DeleteEntireDirectory.cs
@@ -0,0 +1,33 @@
+﻿using System.IO.Abstractions;
+
+namespace Noggog.IO
+{
+    public interface IDeleteEntireDirectory
+    {
+        void DeleteEntireFolder(
+            DirectoryPath dir,
+            bool disableReadOnly = true,
+            bool deleteFolderItself = true);
+    }
+
+    public class DeleteEntireDirectory : IDeleteEntireDirectory
+    {
+        private readonly IFileSystem _fileSystem;
+
+        public DeleteEntireDirectory(IFileSystem fileSystem)
+        {
+            _fileSystem = fileSystem;
+        }
+        
+        public void DeleteEntireFolder(
+            DirectoryPath dir,
+            bool disableReadOnly = true,
+            bool deleteFolderItself = true)
+        {
+            dir.DeleteEntireFolder(
+                    disableReadOnly: disableReadOnly,
+                    deleteFolderItself: deleteFolderItself,
+                    fileSystem: _fileSystem);
+        }
+    }
+}
