78d932b728f8cd0b0e9b7abdaaecd7b0a34b45df
Justin Swanson <justin.c.swanson@gmail.com>
2021-07-29 21:13:25 -0500

    HashCodeExt.AddEnumerable

---
+++ Noggog.CSharpExt/Extensions/HashCodeExt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Noggog
+{
+    public static class HashCodeExt
+    {
+        public static void AddEnumerable<TValue>(this HashCode hashCode, IEnumerable<TValue> col)
+        {
+            foreach (var val in col)
+            {
+                hashCode.Add(val);
+            }
+        }
+    }
+}
