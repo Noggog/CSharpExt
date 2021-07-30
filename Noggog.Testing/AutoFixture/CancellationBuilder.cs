1639d2911875cd4c5f7424fb5927767c7eee2d2b
Justin Swanson <justin.c.swanson@gmail.com>
2021-07-29 21:14:59 -0500

    AutoFixture CancellationBuilder

---
+++ Noggog.Testing/AutoFixture/CancellationBuilder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Reflection;
+using System.Threading;
+using AutoFixture.Kernel;
+
+namespace Noggog.Testing.AutoFixture
+{
+    public class CancellationBuilder : ISpecimenBuilder
+    {
+        public object Create(object request, ISpecimenContext context)
+        {
+            if (request is ParameterInfo p)
+            {
+                if (p.Name == null) return new NoSpecimen();
+                if (p.Name.ContainsInsensitive("cancelled"))
+                {
+                    return new CancellationToken(canceled: true);
+                }
+            }
+            else if (request is Type t)
+            {
+                if (t == typeof(CancellationToken))
+                {
+                    return new CancellationToken(canceled: false);
+                }
+            }
+
+            return new NoSpecimen();
+        }
+    }
+}
