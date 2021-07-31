b52df0ea718cb43f66ef380247861e17e8a45d69
Justin Swanson <justin.c.swanson@gmail.com>
2021-07-31 12:07:39 -0500

    ICurrentDirectoryProvider

---
+++ Noggog.CSharpExt/IO/CurrentDirectoryProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Noggog.IO
+{
+    public interface ICurrentDirectoryProvider
+    {
+        DirectoryPath CurrentDirectory { get; }
+    }
+
+    public class CurrentDirectoryProvider : ICurrentDirectoryProvider
+    {
+        public DirectoryPath CurrentDirectory => Environment.CurrentDirectory;
+    }
+}
