0a027559e321a2252cc2001c5e629cd573e739f7
Justin Swanson <justin.c.swanson@gmail.com>
2021-07-31 00:10:21 -0500

    NoggogModule

---
+++ Noggog.Autofac/Modules/NoggogModule.cs
@@ -0,0 +1,23 @@
+﻿using Autofac;
+using Noggog.IO;
+using Noggog.Utility;
+
+namespace Noggog.Autofac.Modules
+{
+    public class NoggogModule : global::Autofac.Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<TempFileProvider>().As<ITempFileProvider>()
+                .SingleInstance();
+            builder.RegisterType<TempFolderProvider>().As<ITempFolderProvider>()
+                .SingleInstance();
+            builder.RegisterType<ProcessFactory>().As<IProcessFactory>()
+                .SingleInstance();
+            builder.RegisterAssemblyTypes(typeof(IDeepCopyDirectory).Assembly)
+                .InNamespaceOf<IDeleteEntireDirectory>()
+                .AsImplementedInterfaces()
+                .SingleInstance();
+        }
+    }
+}
