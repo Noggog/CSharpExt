a679a27648976c9e146b51fdc81075e666fa3e38
Justin Swanson <justin.c.swanson@gmail.com>
2021-07-30 23:43:41 -0500

    Noggog.IO.DeepCopyDirectory

---
+++ Noggog.CSharpExt/IO/DeepCopyDirectory.cs
@@ -0,0 +1,24 @@
+﻿using System.IO.Abstractions;
+
+namespace Noggog.IO
+{
+    public interface IDeepCopyDirectory
+    {
+        void DeepCopy(DirectoryPath dir, DirectoryPath rhs);
+    }
+
+    public class DeepCopyDirectory : IDeepCopyDirectory
+    {
+        private readonly IFileSystem _fileSystem;
+
+        public DeepCopyDirectory(IFileSystem fileSystem)
+        {
+            _fileSystem = fileSystem;
+        }
+        
+        public void DeepCopy(DirectoryPath dir, DirectoryPath rhs)
+        {
+            _fileSystem.Directory.DeepCopy(dir, rhs);
+        }
+    }
+}
