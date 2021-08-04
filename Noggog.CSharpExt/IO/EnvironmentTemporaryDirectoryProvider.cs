33457343d55b92271da7e95ff39667586f041f3c
Justin Swanson <justin.c.swanson@gmail.com>
2021-08-03 19:43:07 -0500

    EnvironmentTemporaryDirectoryProvider

---
+++ Noggog.CSharpExt/IO/EnvironmentTemporaryDirectoryProvider.cs
@@ -0,0 +1,12 @@
+﻿namespace Noggog.IO
+{
+    public interface IEnvironmentTemporaryDirectoryProvider
+    {
+        DirectoryPath Path { get; }
+    }
+
+    public class EnvironmentTemporaryDirectoryProvider : IEnvironmentTemporaryDirectoryProvider
+    {
+        public DirectoryPath Path => System.IO.Path.GetTempPath();
+    }
+}
