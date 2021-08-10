ac83ff0f17d63331b18dc68f649acb1034124449
Justin Swanson <justin.c.swanson@gmail.com>
2021-08-10 17:00:17 -0500

    Autofac TypicalRegistrations

--- Noggog.Autofac/RegistrationBuilderExt.cs
+++ Noggog.Autofac/RegistrationBuilderExt.cs
@@ -29,6 +29,12 @@ namespace Noggog.Autofac
                 .Where(t => !t.Name.EndsWith("Injection", StringComparison.OrdinalIgnoreCase));
         }
         
+        public static Builder NotDecorator(this Builder registration)
+        {
+            return registration
+                .Where(t => !t.Name.EndsWith("Decorator", StringComparison.OrdinalIgnoreCase));
+        }
+        
         public static Builder AsMatchingInterface(this Builder registration)
         {
             return registration
@@ -36,6 +42,24 @@ namespace Noggog.Autofac
                 {
                     return t.GetInterfaces().Where(x => x.Name == $"I{t.Name}");
                 });
+        }
+        
+        public static Builder TypicalRegistrations(this Builder registration)
+        {
+            return registration
+                .NotInjection()
+                .NotDecorator()
+                .AsMatchingInterface();
+        }
+        
+        public static Builder InstancePerMatchingLifetimeScope(this Builder registration, string? scopeName)
+        {
+            if (scopeName != null)
+            {
+                return registration.InstancePerMatchingLifetimeScope(scopeName);
+            }
+
+            return registration;
         }
     }
 }
