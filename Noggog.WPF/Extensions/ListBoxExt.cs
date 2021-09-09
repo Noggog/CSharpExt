bfe28562fd5495ab51e8e2738a32be8c0a9eae11
Justin Swanson <justin.c.swanson@gmail.com>
2021-09-09 17:09:03 -0500

    ListBoxExt.TryRemoveSelected and IndexOf(ListBoxItem)

---
+++ Noggog.WPF/Extensions/ListBoxExt.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Linq;
+using System.Windows.Controls;
+
+namespace Noggog.WPF
+{
+    public static class ListBoxExt
+    {
+        public static bool TryRemoveSelected(this ListBox listBox)
+        {
+            if (listBox?.ItemsSource is not IList list) return false;
+            foreach (var indexToRemove in listBox.GetChildrenOfType<ListBoxItem>()
+                .WithIndex()
+                .Where(x => x.Item.IsSelected)
+                .Select(x => x.Index)
+                .OrderByDescending(x => x)
+                .ToArray())
+            {
+                list.RemoveAt(indexToRemove);
+            }
+
+            return true;
+        }
+        
+        public static int IndexOf(this ListBox listBox, ListBoxItem listBoxItem)
+        {
+            foreach (var potential in listBox.GetChildrenOfType<ListBoxItem>()
+                .WithIndex())
+            {
+                if (ReferenceEquals(potential.Item, listBoxItem))
+                {
+                    return potential.Index;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
