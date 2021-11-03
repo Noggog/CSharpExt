40854b01b8c1b9aedabd5836cc928e172e5fcb76
Justin Swanson <justin.c.swanson@gmail.com>
2021-11-03 17:39:36 -0500

    Removed faulty ListBoxExt features

--- Noggog.WPF/Extensions/ListBoxExt.cs
+++ Noggog.WPF/Extensions/ListBoxExt.cs
@@ -6,37 +6,5 @@ namespace Noggog.WPF
 {
     public static class ListBoxExt
     {
-        public static bool TryRemoveSelected(this ListBox listBox)
-        {
-            if (listBox?.ItemsSource is not IList list) return false;
-            foreach (var indexToRemove in listBox.GetChildrenOfType<ListBoxItem>()
-                .WithIndex()
-                .Where(x => x.Item.IsSelected)
-                .Select(x => x.Index)
-                .OrderByDescending(x => x)
-                .ToArray())
-            {
-                list.RemoveAt(indexToRemove);
-            }
-
-            return true;
-        }
-        
-        public static int IndexOf(this ListBox listBox, ListBoxItem listBoxItem)
-        {
-            var depth = listBox.GetChildDepth<ListBoxItem>();
-            if (depth == -1) return -1;
-
-            foreach (var potential in listBox.GetChildrenOfType<ListBoxItem>(maxDepth: depth)
-                .WithIndex())
-            {
-                if (ReferenceEquals(potential.Item, listBoxItem))
-                {
-                    return potential.Index;
-                }
-            }
-
-            return -1;
-        }
     }
 }
