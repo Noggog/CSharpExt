aa0002d81bb4aecd890b91c08a9ed168e93a6bea
Justin Swanson <justin.c.swanson@gmail.com>
2021-11-11 19:52:40 -0600

    Cache.Empty

--- Noggog.CSharpExt/Containers/Cache.cs
+++ Noggog.CSharpExt/Containers/Cache.cs
@@ -2,46 +2,47 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
-using System.Text;
 
 namespace Noggog
 {
     public class Cache<TObject, TKey> : ICache<TObject, TKey>
         where TKey : notnull
     {
+        public static readonly IReadOnlyCache<TObject, TKey> Empty = new Cache<TObject, TKey>(_ => default!);
+
         private readonly Func<TObject, TKey> _keySelector;
-        private Dictionary<TKey, TObject> _dict = new Dictionary<TKey, TObject>();
+        private Dictionary<TKey, TObject> _dict = new();
 
-        public TObject this[TKey key] => this._dict[key];
+        public TObject this[TKey key] => _dict[key];
 
-        public IEnumerable<TKey> Keys => this._dict.Keys;
+        public IEnumerable<TKey> Keys => _dict.Keys;
 
-        public int Count => this._dict.Count;
+        public int Count => _dict.Count;
 
-        public IEnumerable<TObject> Items => this._dict.Values;
+        public IEnumerable<TObject> Items => _dict.Values;
 
-        public IEnumerable<KeyValuePair<TKey, TObject>> KeyValues => this._dict;
+        public IEnumerable<KeyValuePair<TKey, TObject>> KeyValues => _dict;
 
         public Cache(Func<TObject, TKey> keySelector)
         {
-            this._keySelector = keySelector ?? throw new ArgumentNullException(nameof(keySelector));
+            _keySelector = keySelector ?? throw new ArgumentNullException(nameof(keySelector));
         }
 
-        public void Clear() => this._dict.Clear();
+        public void Clear() => _dict.Clear();
 
-        public bool ContainsKey(TKey key) => this._dict.ContainsKey(key);
+        public bool ContainsKey(TKey key) => _dict.ContainsKey(key);
 
-        public bool Remove(TKey key) => this._dict.Remove(key);
+        public bool Remove(TKey key) => _dict.Remove(key);
 
         public void Remove(IEnumerable<TKey> keys)
         {
             foreach (var key in keys)
             {
-                this._dict.Remove(key);
+                _dict.Remove(key);
             }
         }
 
-        public bool TryGetValue(TKey key, [MaybeNullWhen(false)] out TObject value) => this._dict.TryGetValue(key, out value);
+        public bool TryGetValue(TKey key, [MaybeNullWhen(false)] out TObject value) => _dict.TryGetValue(key, out value);
         
         public TObject? TryGetValue(TKey key)
         {
@@ -55,79 +56,79 @@ namespace Noggog
         
         public void Refresh()
         {
-            var tmp = this._dict;
-            this._dict = new Dictionary<TKey, TObject>();
+            var tmp = _dict;
+            _dict = new Dictionary<TKey, TObject>();
             foreach (var item in tmp.Values)
             {
-                this._dict[_keySelector(item)] = item;
+                _dict[_keySelector(item)] = item;
             }
         }
 
         public void Refresh(IEnumerable<TKey> keys)
         {
-            List<TObject> objs = new List<TObject>();
+            List<TObject> objs = new();
             foreach (var key in keys)
             {
-                if (this._dict.TryGetValue(key, out var val))
+                if (_dict.TryGetValue(key, out var val))
                 {
                     objs.Add(val);
-                    this._dict.Remove(key);
+                    _dict.Remove(key);
                 }
             }
             foreach (var obj in objs)
             {
-                this._dict[this._keySelector(obj)] = obj;
+                _dict[_keySelector(obj)] = obj;
             }
         }
 
         public void Refresh(TKey key)
         {
-            if (!this.TryGetValue(key, out var val)) return;
-            this._dict.Remove(key);
-            this._dict[this._keySelector(val)] = val;
+            if (!TryGetValue(key, out var val)) return;
+            _dict.Remove(key);
+            _dict[_keySelector(val)] = val;
         }
 
         public IEnumerator<IKeyValue<TObject, TKey>> GetEnumerator()
         {
-            foreach (var item in this._dict)
+            foreach (var item in _dict)
             {
                 yield return new KeyValue<TObject, TKey>(item.Key, item.Value);
             }
         }
 
         IEnumerator<IKeyValue<TObject, TKey>> IEnumerable<IKeyValue<TObject, TKey>>.GetEnumerator() => this.GetEnumerator();
-        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 
-        public void Set(TObject item) => this._dict[this._keySelector(item)] = item;
+        public void Set(TObject item) => _dict[_keySelector(item)] = item;
 
         public void Set(IEnumerable<TObject> items)
         {
             foreach (var item in items)
             {
-                this._dict[this._keySelector(item)] = item;
+                _dict[_keySelector(item)] = item;
             }
         }
 
         public void Add(TObject item)
         {
-            this._dict.Add(this._keySelector(item), item);
+            _dict.Add(_keySelector(item), item);
         }
 
-        public bool Remove(TObject obj) => this._dict.Remove(this._keySelector(obj));
+        public bool Remove(TObject obj) => _dict.Remove(_keySelector(obj));
 
         public void Remove(IEnumerable<TObject> objects)
         {
             foreach (var item in objects)
             {
-                this._dict.Remove(this._keySelector(item));
+                _dict.Remove(_keySelector(item));
             }
         }
 
         public TObject GetOrAdd(TKey key, Func<TKey, TObject> createFunc)
         {
-            if (this._dict.TryGetValue(key, out var val)) return val;
+            if (_dict.TryGetValue(key, out var val)) return val;
             val = createFunc(key);
-            this.Set(val);
+            Set(val);
             return val;
         }
     }
