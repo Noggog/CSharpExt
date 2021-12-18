4e6fbb63432b00c94c4ba0ad7688311512c657e5
Justin Swanson <justin.c.swanson@gmail.com>
2021-12-18 15:36:40 -0600

    DirectoryPath/FilePath Json serialization tests

---
+++ CSharpExt.UnitTests/IO/DirectoryPathJsonConverterTests.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using FluentAssertions;
+using Newtonsoft.Json;
+using Noggog;
+using Xunit;
+
+namespace CSharpExt.UnitTests.IO;
+
+public class DirectoryPathJsonConverterTests
+{
+    class Dto
+    {
+        public DirectoryPath MyDirectory { get; set; }
+    }
+    
+    [Fact]
+    public void NakedSerialization()
+    {
+        var str = File.ReadAllText(Path.Combine("IO", "Files", "NakedDirectoryPathSerialization.json"));
+        var converted = JsonConvert.DeserializeObject<Dto>(str)!;
+        converted.MyDirectory.RelativePath.Should().Be("C:\\SomeDir\\SomePath");
+    }
+}
---
+++ CSharpExt.UnitTests/IO/FilePathJsonConverterTests.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using FluentAssertions;
+using Newtonsoft.Json;
+using Noggog;
+using Xunit;
+
+namespace CSharpExt.UnitTests.IO;
+
+public class FilePathJsonConverterTests
+{
+    class Dto
+    {
+        public FilePath MyFile { get; set; }
+    }
+    
+    [Fact]
+    public void NakedSerialization()
+    {
+        var str = File.ReadAllText(Path.Combine("IO", "Files", "NakedFilePathSerialization.json"));
+        var converted = JsonConvert.DeserializeObject<Dto>(str)!;
+        converted.MyFile.RelativePath.Should().Be("C:\\SomeDir\\SomeFile.txt");
+    }
+}
