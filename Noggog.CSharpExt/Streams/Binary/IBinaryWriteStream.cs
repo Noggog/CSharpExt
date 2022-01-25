0d67616210045a7dc5c77316281ff9af7b724981
Justin Swanson <justin.c.swanson@gmail.com>
2022-01-24 20:08:51 -0600

    IBinaryWriteStream.Write(ReadOnlySpan<char>) removed

--- Noggog.CSharpExt/Streams/Binary/IBinaryWriteStream.cs
+++ Noggog.CSharpExt/Streams/Binary/IBinaryWriteStream.cs
@@ -1,36 +1,27 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Noggog
+namespace Noggog;
+
+public interface IBinaryWriteStream : IDisposable
 {
-    public interface IBinaryWriteStream : IDisposable
-    {
-        long Position { get; set; }
-        long Length { get; }
-        bool IsLittleEndian { get; }
-        Stream BaseStream { get; }
+    long Position { get; set; }
+    long Length { get; }
+    bool IsLittleEndian { get; }
+    Stream BaseStream { get; }
 #if NETSTANDARD2_0
 #else
-        void Write(ReadOnlySpan<byte> buffer);
+    void Write(ReadOnlySpan<byte> buffer);
 #endif
-        void Write(bool value);
-        void Write(byte value);
-        void Write(ushort value);
-        void Write(uint value);
-        void Write(ulong value);
-        void Write(sbyte value);
-        void Write(short value);
-        void Write(int value);
-        void Write(long value);
-        void Write(float value);
-        void Write(double value);
-#if NETSTANDARD2_0
-#else
-        void Write(ReadOnlySpan<char> value);
-#endif
-    }
+    void Write(bool value);
+    void Write(byte value);
+    void Write(ushort value);
+    void Write(uint value);
+    void Write(ulong value);
+    void Write(sbyte value);
+    void Write(short value);
+    void Write(int value);
+    void Write(long value);
+    void Write(float value);
+    void Write(double value);
 }
