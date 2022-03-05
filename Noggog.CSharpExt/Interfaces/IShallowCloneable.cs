4328e8b0b4b0cdc6568f9999b9776472dc5d24eb
Justin Swanson <justin.c.swanson@gmail.com>
2022-03-05 15:17:10 -0600

    IShallowCloneable /w typed generic

--- Noggog.CSharpExt/Interfaces/IShallowCloneable.cs
+++ Noggog.CSharpExt/Interfaces/IShallowCloneable.cs
@@ -1,8 +1,13 @@
-﻿using System;
+using System;
 
 namespace Noggog;
 
 public interface IShallowCloneable
 {
     object ShallowClone();
+}
+
+public interface IShallowCloneable<T> : IShallowCloneable
+{
+    new T ShallowClone();
 }
