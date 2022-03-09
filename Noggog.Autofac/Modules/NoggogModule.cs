f5db921d28c00c846fde4a028996fb2edf46c258
Justin Swanson <justin.c.swanson@gmail.com>
2022-03-08 18:16:22 -0600

    IWatchFile added to module

--- Noggog.Autofac/Modules/NoggogModule.cs
+++ Noggog.Autofac/Modules/NoggogModule.cs
@@ -1,27 +1,28 @@
 ﻿using Autofac;
 using Noggog.IO;
+using Noggog.Reactive;
 using Noggog.Time;
 using Noggog.Utility;
 
-namespace Noggog.Autofac.Modules
+namespace Noggog.Autofac.Modules;
+
+public class NoggogModule : global::Autofac.Module
 {
-    public class NoggogModule : global::Autofac.Module
+    protected override void Load(ContainerBuilder builder)
     {
-        protected override void Load(ContainerBuilder builder)
-        {
-            builder.RegisterType<TempFileProvider>().As<ITempFileProvider>()
-                .SingleInstance();
-            builder.RegisterType<TempFolderProvider>().As<ITempFolderProvider>()
-                .SingleInstance();
-            builder.RegisterType<ProcessFactory>().As<IProcessFactory>()
-                .SingleInstance();
-            builder.RegisterAssemblyTypes(typeof(IDeepCopyDirectory).Assembly)
-                .InNamespacesOf(
-                    typeof(IDeleteEntireDirectory),
-                    typeof(INowProvider))
-                .NotInjection()
-                .AsImplementedInterfaces()
-                .SingleInstance();
-        }
+        builder.RegisterType<TempFileProvider>().As<ITempFileProvider>()
+            .SingleInstance();
+        builder.RegisterType<TempFolderProvider>().As<ITempFolderProvider>()
+            .SingleInstance();
+        builder.RegisterType<ProcessFactory>().As<IProcessFactory>()
+            .SingleInstance();
+        builder.RegisterAssemblyTypes(typeof(IDeepCopyDirectory).Assembly)
+            .InNamespacesOf(
+                typeof(IDeleteEntireDirectory),
+                typeof(INowProvider),
+                typeof(IWatchFile))
+            .NotInjection()
+            .AsImplementedInterfaces()
+            .SingleInstance();
     }
 }
