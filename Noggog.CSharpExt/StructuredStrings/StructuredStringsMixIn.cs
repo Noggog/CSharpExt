0f38f288598b3bf7c393c1e052d910a98f6b9718
Justin Swanson <justin.c.swanson@gmail.com>
2022-05-04 20:34:26 -0500

    IPrintable.Print() extension method

---
+++ Noggog.CSharpExt/StructuredStrings/StructuredStringsMixIn.cs
@@ -0,0 +1,14 @@
+﻿using System.IO.Abstractions;
+
+namespace Noggog.StructuredStrings;
+
+public static class StructuredStringsMixIn
+{
+    public static string Print(this IPrintable printable, string? name = null)
+    {
+        StructuredStringBuilder sb = new();
+        printable.Print(sb, name);
+        return sb.ToString();
+    }
+    
+}
