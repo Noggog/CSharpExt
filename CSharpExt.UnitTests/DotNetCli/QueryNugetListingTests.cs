c3ed9b069a75ff45f0934c26705697c0598b5930
Justin Swanson <justin.c.swanson@gmail.com>
2022-08-04 17:00:20 -0500

    Migrated some more unit tests up

---
+++ CSharpExt.UnitTests/DotNetCli/QueryNugetListingTests.cs
@@ -0,0 +1,82 @@
+﻿using FluentAssertions;
+using Noggog;
+using Noggog.DotNetCli.DI;
+using Noggog.Processes;
+using NSubstitute;
+using Xunit;
+
+namespace CSharpExt.UnitTests.DotNetCli;
+
+public class QueryNugetListingTests
+{
+    [Theory, BasicAutoData]
+    public async Task CallsConstructListWithProjPath(
+        FilePath projPath,
+        CancellationToken cancel,
+        QueryNugetListing sut)
+    {
+        sut.ProcessRunner.RunAndCapture(default!, default).ReturnsForAnyArgs(new ProcessResult());
+        await sut.Query(projPath, default, default, default, cancel);
+        sut.NetCommandStartConstructor.Received(1).Construct("list", projPath, Arg.Any<string[]>());
+    }
+        
+    [Theory, NoggogInlineData(true), NoggogInlineData(false)]
+    public async Task ConstructListRespectsOutdated(
+        bool outdated,
+        FilePath projPath,
+        CancellationToken cancel,
+        QueryNugetListing sut)
+    {
+        sut.ProcessRunner.RunAndCapture(default!, default).ReturnsForAnyArgs(new ProcessResult());
+        string[]? passedArgs = null;
+        sut.NetCommandStartConstructor.Construct(Arg.Any<string>(), Arg.Any<FilePath>(),
+            Arg.Do<string[]>(x => passedArgs = x));
+        await sut.Query(projPath, default, outdated: outdated, default, cancel);
+        if (outdated)
+        {
+            passedArgs.Should().Contain("--outdated");
+        }
+        else
+        {
+            passedArgs.Should().NotContain("--outdated");
+        }
+    }
+        
+    [Theory, NoggogInlineData(true), NoggogInlineData(false)]
+    public async Task ConstructListRespectsIncludePrerelease(
+        bool inclPrerelease,
+        FilePath projPath,
+        CancellationToken cancel,
+        QueryNugetListing sut)
+    {
+        sut.ProcessRunner.RunAndCapture(default!, default).ReturnsForAnyArgs(new ProcessResult());
+        string[]? passedArgs = null;
+        sut.NetCommandStartConstructor.Construct(Arg.Any<string>(), Arg.Any<FilePath>(),
+            Arg.Do<string[]>(x => passedArgs = x));
+        await sut.Query(projPath, default, default, includePrerelease: inclPrerelease, cancel);
+        if (inclPrerelease)
+        {
+            passedArgs.Should().Contain("--include-prerelease");
+        }
+        else
+        {
+            passedArgs.Should().NotContain("--include-prerelease");
+        }
+    }
+
+    [Theory, BasicAutoData]
+    public async Task PassesResultsToProcessor(
+        FilePath projPath,
+        CancellationToken cancel,
+        List<string> processOutput,
+        IEnumerable<NugetListingQuery> functionReturn,
+        QueryNugetListing sut)
+    {
+        var processReturn = new ProcessResult(0, processOutput, new List<string>());
+        sut.ProcessRunner.RunAndCapture(default!, default).ReturnsForAnyArgs(processReturn);
+        sut.ResultProcessor.Process(processReturn.Out).Returns(functionReturn);
+        var result = await sut.Query(projPath, default, default, default, cancel);
+        result.Succeeded.Should().BeTrue();
+        result.Value.Should().Equal(functionReturn);
+    }
+}
---
+++ CSharpExt.UnitTests/Processes/ProcessRunnerTests.cs
@@ -0,0 +1,221 @@
+﻿using System.Diagnostics;
+using System.Reactive.Linq;
+using AutoFixture.Xunit2;
+using FluentAssertions;
+using Noggog.Processes;
+using Noggog.Processes.DI;
+using NSubstitute;
+using Xunit;
+
+namespace CSharpExt.UnitTests.Processes;
+
+public class ProcessRunnerTests
+{
+    #region Run
+
+    [Theory, BasicAutoData(ConfigureMembers: false)]
+    public async Task TypicalRun_StartInfoPassedToFactory(
+        [Frozen]ProcessStartInfo startInfo,
+        CancellationToken cancel,
+        ProcessRunner sut)
+    {
+        await sut.Run(startInfo, cancel);
+        sut.Factory.Received(1).Create(startInfo, cancel);
+    }
+
+    [Theory, BasicAutoData(ConfigureMembers: false)]
+    public async Task TypicalRun_FactoryResultIsRunAndReturned(
+        int ret,
+        [Frozen]ProcessStartInfo startInfo,
+        IProcessWrapper process,
+        CancellationToken cancel,
+        ProcessRunner sut)
+    {
+        process.Run().Returns(Task.FromResult(ret));
+        sut.Factory.Create(default!).ReturnsForAnyArgs(process);
+        (await sut.Run(startInfo, cancel))
+            .Should().Be(ret);
+    }
+
+    #endregion
+
+    #region RunAndCapture
+
+    [Theory, BasicAutoData]
+    public async Task RunAndCapture_CallsFactory(
+        ProcessStartInfo startInfo,
+        CancellationToken cancel,
+        ProcessRunner sut)
+    {
+        await sut.RunAndCapture(startInfo, cancel);
+        sut.Factory.Received(1).Create(startInfo, cancel);
+    }
+        
+    [Theory, BasicAutoData(ConfigureMembers: false)]
+    public async Task RunAndCapture_PutsOutIntoOut(
+        string str,
+        [Frozen]ProcessStartInfo startInfo,
+        IProcessWrapper process,
+        CancellationToken cancel,
+        ProcessRunner sut)
+    {
+        process.Output.Returns(Observable.Return(str));
+        sut.Factory.Create(default!).ReturnsForAnyArgs(process);
+        var result = await sut.RunAndCapture(startInfo, cancel);
+        result.Out.Should().Equal(str);
+    }
+        
+    [Theory, BasicAutoData(ConfigureMembers: false)]
+    public async Task RunAndCapture_PutsErrIntoErr(
+        string str,
+        [Frozen]ProcessStartInfo startInfo,
+        IProcessWrapper process,
+        CancellationToken cancel,
+        ProcessRunner sut)
+    {
+        process.Error.Returns(Observable.Return(str));
+        sut.Factory.Create(default!).ReturnsForAnyArgs(process);
+        var result = await sut.RunAndCapture(startInfo, cancel);
+        result.Errors.Should().Equal(str);
+    }
+        
+    [Theory, BasicAutoData(ConfigureMembers: false)]
+    public async Task RunAndCapture_ReturnsProcessReturn(
+        int ret,
+        [Frozen]ProcessStartInfo startInfo,
+        IProcessWrapper process,
+        CancellationToken cancel,
+        ProcessRunner sut)
+    {
+        process.Run().Returns(Task.FromResult(ret));
+        sut.Factory.Create(default!).ReturnsForAnyArgs(process);
+        (await sut.RunAndCapture(startInfo, cancel))
+            .Result.Should().Be(ret);
+    }
+
+    #endregion
+
+    #region RunWithCallbacks
+
+    [Theory, BasicAutoData]
+    public async Task RunWithCallbacks_CallsFactory(
+        ProcessStartInfo startInfo,
+        CancellationToken cancel,
+        Action<string> callback,
+        ProcessRunner sut)
+    {
+        await sut.RunWithCallback(startInfo, callback, callback, cancel);
+        sut.Factory.Received(1).Create(startInfo, cancel);
+    }
+        
+    [Theory, BasicAutoData(ConfigureMembers: false)]
+    public async Task RunWithCallbacks_CallsOutCallback(
+        string str,
+        [Frozen]ProcessStartInfo startInfo,
+        IProcessWrapper process,
+        CancellationToken cancel,
+        Action<string> errCb,
+        ProcessRunner sut)
+    {
+        process.Output.Returns(Observable.Return(str));
+        sut.Factory.Create(default!).ReturnsForAnyArgs(process);
+        var received = new List<string>();
+        await sut.RunWithCallback(startInfo, received.Add, errCb, cancel);
+        received.Should().Equal(str);
+    }
+        
+    [Theory, BasicAutoData(ConfigureMembers: false)]
+    public async Task RunWithCallbacks_PutsErrIntoErr(
+        string str,
+        [Frozen]ProcessStartInfo startInfo,
+        IProcessWrapper process,
+        Action<string> outCb,
+        CancellationToken cancel,
+        ProcessRunner sut)
+    {
+        process.Error.Returns(Observable.Return(str));
+        sut.Factory.Create(default!).ReturnsForAnyArgs(process);
+        var received = new List<string>();
+        await sut.RunWithCallback(startInfo, outCb, received.Add, cancel);
+        received.Should().Equal(str);
+    }
+        
+    [Theory, BasicAutoData(ConfigureMembers: false)]
+    public async Task RunWithCallbacks_ReturnsProcessReturn(
+        int ret,
+        [Frozen]ProcessStartInfo startInfo,
+        IProcessWrapper process,
+        Action<string> outCb,
+        Action<string> errCb,
+        CancellationToken cancel,
+        ProcessRunner sut)
+    {
+        process.Run().Returns(Task.FromResult(ret));
+        sut.Factory.Create(default!).ReturnsForAnyArgs(process);
+        (await sut.RunWithCallback(startInfo, outCb, errCb, cancel))
+            .Should().Be(ret);
+    }
+
+    #endregion
+
+    #region RunWithCallback
+
+
+    [Theory, BasicAutoData]
+    public async Task RunWithCallback_CallsFactory(
+        ProcessStartInfo startInfo,
+        CancellationToken cancel,
+        Action<string> callback,
+        ProcessRunner sut)
+    {
+        await sut.RunWithCallback(startInfo, callback, cancel);
+        sut.Factory.Received(1).Create(startInfo, cancel);
+    }
+        
+    [Theory, BasicAutoData(ConfigureMembers: false)]
+    public async Task RunWithCallback_CallsOutCallback(
+        string str,
+        [Frozen]ProcessStartInfo startInfo,
+        IProcessWrapper process,
+        CancellationToken cancel,
+        ProcessRunner sut)
+    {
+        process.Output.Returns(Observable.Return(str));
+        sut.Factory.Create(default!).ReturnsForAnyArgs(process);
+        var received = new List<string>();
+        await sut.RunWithCallback(startInfo, received.Add, cancel);
+        received.Should().Equal(str);
+    }
+        
+    [Theory, BasicAutoData(ConfigureMembers: false)]
+    public async Task RunWithCallback_PutsErrIntoCallback(
+        string str,
+        [Frozen]ProcessStartInfo startInfo,
+        IProcessWrapper process,
+        CancellationToken cancel,
+        ProcessRunner sut)
+    {
+        process.Error.Returns(Observable.Return(str));
+        sut.Factory.Create(default!).ReturnsForAnyArgs(process);
+        var received = new List<string>();
+        await sut.RunWithCallback(startInfo, received.Add, cancel);
+        received.Should().Equal(str);
+    }
+        
+    [Theory, BasicAutoData(ConfigureMembers: false)]
+    public async Task RunWithCallback_ReturnsProcessReturn(
+        int ret,
+        [Frozen]ProcessStartInfo startInfo,
+        IProcessWrapper process,
+        Action<string> callback,
+        CancellationToken cancel,
+        ProcessRunner sut)
+    {
+        process.Run().Returns(Task.FromResult(ret));
+        sut.Factory.Create(default!).ReturnsForAnyArgs(process);
+        (await sut.RunWithCallback(startInfo, callback, cancel))
+            .Should().Be(ret);
+    }
+
+    #endregion
+}
