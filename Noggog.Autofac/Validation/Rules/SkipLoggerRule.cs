80d306181ca63da0e2b1dbbee182a01c4cd1a592
Justin Swanson <justin.c.swanson@gmail.com>
2022-08-07 15:25:50 -0500

    SkipLoggerRule

---
+++ Noggog.Autofac/Validation/Rules/SkipLoggerRule.cs
@@ -0,0 +1,12 @@
+﻿namespace Noggog.Autofac.Validation.Rules;
+
+public class SkipLoggerRule : IValidationRule
+{
+    // To Do
+    // Find a more robust way to validate logging systems
+    public bool IsAllowed(Type type)
+    {
+        if (type.Name.StartsWith("ILogger")) return true;
+        return false;
+    }
+}
