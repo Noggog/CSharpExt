d900e58bc3f1114bb5afcd55382d0474e572ccba
Justin Swanson <justin.c.swanson@gmail.com>
2022-09-23 18:04:18 -0500

    ExceptionExt moved up from Loqui

---
+++ Noggog.CSharpExt/Extensions/ExceptionExt.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+namespace Noggog;
+
+public static class ExceptionExt
+{
+    [return: NotNullIfNotNull("lhs")]
+    [return: NotNullIfNotNull("rhs")]
+    public static Exception? Combine(this Exception? lhs, Exception? rhs)
+    {
+        if (lhs == null && rhs == null) return null;
+        if (lhs == null) return rhs;
+        if (rhs == null) return lhs;
+        var lhsAgg = lhs as AggregateException;
+        var rhsAgg = rhs as AggregateException;
+        if (lhsAgg != null && rhsAgg != null)
+        {
+            return new AggregateException(
+                lhsAgg.InnerExceptions.Select(i => i).And(rhsAgg.InnerExceptions));
+        }
+        if (lhsAgg != null)
+        {
+            return new AggregateException(
+                lhsAgg.InnerExceptions.And(rhs));
+        }
+        if (rhsAgg != null)
+        {
+            return new AggregateException(
+                rhsAgg.InnerExceptions.And(lhs));
+        }
+        return new AggregateException(
+            lhs,
+            rhs);
+    }
+
+    [return: NotNullIfNotNull("lhs")]
+    [return: NotNullIfNotNull("rhs")]
+    public static IEnumerable<T>? Combine<T>(this IEnumerable<T>? lhs, IEnumerable<T>? rhs)
+    {
+        if (lhs == null && rhs == null) return null;
+        if (rhs == null) return lhs;
+        if (lhs == null) return rhs;
+        return lhs.And(rhs);
+    }
+}
