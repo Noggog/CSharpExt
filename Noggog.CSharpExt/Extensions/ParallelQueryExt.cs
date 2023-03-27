a85e136195e73bfedceb35dc598f4c52c7a3deac
Justin Swanson <justin.c.swanson@gmail.com>
2023-03-26 22:22:18 -0500

    ParallelQueryExt.NotNull

---
+++ Noggog.CSharpExt/Extensions/ParallelQueryExt.cs
@@ -0,0 +1,17 @@
+﻿namespace Noggog.Extensions;
+
+public static class ParallelQueryExt
+{
+    public static IEnumerable<T> NotNull<T>(this ParallelQuery<T?> e)
+        where T : class
+    {
+        return e.Where(i => i != null)!;
+    }
+
+    public static IEnumerable<T> NotNull<T>(this ParallelQuery<T?> e)
+        where T : struct
+    {
+        return e.Where(i => i.HasValue)
+            .Select(i => i!.Value);
+    }
+}
