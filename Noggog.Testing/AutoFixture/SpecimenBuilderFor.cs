6e20a8e6b6ef967ed7c36771e6eceabe04578dd4
Justin Swanson <justin.c.swanson@gmail.com>
2023-04-21 23:26:35 -0500

    SpecimenBuilderFor

---
+++ Noggog.Testing/AutoFixture/SpecimenBuilderFor.cs
@@ -0,0 +1,19 @@
+﻿using AutoFixture.Kernel;
+
+namespace Noggog.Testing.AutoFixture;
+
+public abstract class SpecimenBuilderFor<T> : ISpecimenBuilder
+    where T : notnull
+{
+    public object Create(object request, ISpecimenContext context)
+    {
+        if (request is Type t && t == typeof(T))
+        {
+            return Create(context);
+        }
+    
+        return new NoSpecimen();
+    }
+
+    public abstract T Create(ISpecimenContext context);
+}
