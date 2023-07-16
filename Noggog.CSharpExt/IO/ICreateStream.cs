3e7e78ccc25205a5a1f57c46beaa0c1fe065708d
Justin Swanson <justin.c.swanson@gmail.com>
2023-07-15 22:48:58 -0500

    ICreateStream

---
+++ Noggog.CSharpExt/IO/ICreateStream.cs
@@ -0,0 +1,39 @@
+﻿using System.IO.Abstractions;
+
+namespace Noggog.IO;
+
+public interface ICreateStream
+{
+    Stream GetStreamFor(IFileSystem fileSystem, FilePath path, bool write);
+}
+
+public class NormalFileStreamCreator : ICreateStream
+{
+    public static readonly NormalFileStreamCreator Instance = new();
+    
+    public Stream GetStreamFor(IFileSystem fileSystem, FilePath path, bool write)
+    {
+        return fileSystem.File.Open(path, write ? FileMode.Create : FileMode.Open, write ? FileAccess.Write : FileAccess.Read, FileShare.Read);
+    }
+}
+
+public class NoPreferenceStreamCreator : ICreateStream
+{
+    public Stream GetStreamFor(IFileSystem fileSystem, FilePath path, bool write)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public static class ICreateStreamExt
+{
+    public static ICreateStream GetOrFallback(this ICreateStream? createStream, Func<ICreateStream> fallback)
+    {
+        if (createStream == null || createStream is NoPreferenceStreamCreator)
+        {
+            return fallback();
+        }
+
+        return createStream;
+    }
+}
