e86bc8013faaa7aa4d7aa0412aca800263c802b4
Justin Swanson <justin.c.swanson@gmail.com>
2024-11-18 23:48:22 -0600

    IProcessRunner killWithParent parameter exposed

--- Noggog.CSharpExt/Processes/DI/ProcessRunner.cs
+++ Noggog.CSharpExt/Processes/DI/ProcessRunner.cs
@@ -6,22 +6,26 @@ public interface IProcessRunner
 {
     Task<ProcessResult> RunAndCapture(
         ProcessStartInfo startInfo,
-        CancellationToken cancel = default);
+        CancellationToken cancel = default,
+        bool killWithParent = true);
         
     Task<int> Run(
         ProcessStartInfo startInfo,
-        CancellationToken cancel = default);
+        CancellationToken cancel = default,
+        bool killWithParent = true);
 
     Task<int> RunWithCallback(
         ProcessStartInfo startInfo,
         Action<string> outputCallback,
         Action<string> errorCallback,
-        CancellationToken cancel = default);
+        CancellationToken cancel = default,
+        bool killWithParent = true);
 
     Task<int> RunWithCallback(
         ProcessStartInfo startInfo,
         Action<string> callback,
-        CancellationToken cancel = default);
+        CancellationToken cancel = default,
+        bool killWithParent = true);
 }
 
 public class ProcessRunner : IProcessRunner
@@ -33,10 +37,10 @@ public class ProcessRunner : IProcessRunner
         Factory = processFactory;
     }
     
-        
     public async Task<ProcessResult> RunAndCapture(
         ProcessStartInfo startInfo,
-        CancellationToken cancel = default)
+        CancellationToken cancel = default,
+        bool killWithParent = true)
     {
         var outs = new List<string>();
         var errs = new List<string>();
@@ -44,17 +48,20 @@ public class ProcessRunner : IProcessRunner
             startInfo,
             i => outs.Add(i),
             i => errs.Add(i),
-            cancel).ConfigureAwait(false);
+            cancel,
+            killWithParent: killWithParent).ConfigureAwait(false);
         return new(ret, outs, errs);
     }
 
     public async Task<int> Run(
         ProcessStartInfo startInfo,
-        CancellationToken cancel = default)
+        CancellationToken cancel = default,
+        bool killWithParent = true)
     {
         using var proc = Factory.Create(
             startInfo,
-            cancel: cancel);
+            cancel: cancel,
+            killWithParent: killWithParent);
         return await proc.Run().ConfigureAwait(false);
     }
 
@@ -62,11 +69,13 @@ public class ProcessRunner : IProcessRunner
         ProcessStartInfo startInfo,
         Action<string> outputCallback, 
         Action<string> errorCallback,
-        CancellationToken cancel = default)
+        CancellationToken cancel = default,
+        bool killWithParent = true)
     {
         using var proc = Factory.Create(
             startInfo,
-            cancel: cancel);
+            cancel: cancel,
+            killWithParent: killWithParent);
         using var outSub = proc.Output.Subscribe(outputCallback);
         List<string> errs = new();
         using var errSub = proc.Error.Subscribe(errs.Add);
@@ -86,12 +95,14 @@ public class ProcessRunner : IProcessRunner
     public async Task<int> RunWithCallback(
         ProcessStartInfo startInfo,
         Action<string> callback,
-        CancellationToken cancel = default)
+        CancellationToken cancel = default,
+        bool killWithParent = true)
     {
         return await RunWithCallback(
             startInfo,
             callback,
             callback,
-            cancel).ConfigureAwait(false);;
+            cancel,
+            killWithParent: killWithParent).ConfigureAwait(false);;
     }
 }
