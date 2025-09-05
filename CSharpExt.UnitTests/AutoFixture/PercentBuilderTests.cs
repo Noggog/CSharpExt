c5b965a4635b572013e0eaab49659fbf9fcfd0a7
Justin Swanson <justin.c.swanson@gmail.com>
2025-09-05 02:36:28 -0500

    PercentBuilder

---
+++ CSharpExt.UnitTests/AutoFixture/PercentBuilderTests.cs
@@ -0,0 +1,14 @@
+﻿using AutoFixture.Kernel;
+using Noggog;
+using Noggog.Testing.AutoFixture;
+
+namespace CSharpExt.UnitTests.AutoFixture;
+
+public class PercentBuilderTests
+{
+    [Theory, DefaultAutoData]
+    public void Typical(
+        Percent sut)
+    {
+    }
+}
---
+++ Noggog.Testing/AutoFixture/PercentBuilder.cs
@@ -0,0 +1,19 @@
+﻿using AutoFixture;
+using AutoFixture.Kernel;
+
+namespace Noggog.Testing.AutoFixture;
+
+public class PercentBuilder : ISpecimenBuilder
+{
+    private readonly Random _random = new();
+    
+    public object Create(object request, ISpecimenContext context)
+    {
+        if (request is Type t
+            && t == typeof(Percent))
+        {
+            return new Percent(_random.NextDouble());
+        }
+        return new NoSpecimen();
+    }
+}
